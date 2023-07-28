d149383e931bb221db76d9dfea6c3df97beb4932
DarnellMLambert <Darnellmarquanlambert@gmail.com>
2023-07-28 11:25:59 -0500

    New relative path

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,27 +5,29 @@
 
 import os
 import sys
-# from urllib.parse import quote
-
+#from urllib.parse import quote
+#
 #password = '@Darn12213529L'
 #encoded_password = quote(password)
 #
-#sys.path.insert(0, os.path.abspath('./inputsIndex'))
+# sys.path.insert(0, os.path.abspath('./inputsIndex'))
 #
 #sqltable_connection_string = 'mysql+pymysql://root:{encoded_password}@localhost:3306/variables'
-current_dir = os.getcwd()
+#current_dir = os.getcwd()
+#
+#relative_path=r'.\inputsIndex'
+#
+#relative_path_to_main_py = r'..\src\Main.py'
+#
+#absolute_path = os.path.join(current_dir, relative_path)
+#
+#accert_path = os.path.join(current_dir, relative_path_to_main_py)
+#
+#sys.path.insert(0, os.path.abspath(absolute_path))
+#
+#sys.path.append(accert_path)
 
-relative_path=r'.\inputsIndex'
-
-relative_path_to_main_py = r'.\inputsIndex\Main.py'
-
-absolute_path = os.path.join(current_dir, relative_path)
-
-accert_path = os.path.join(current_dir, relative_path_to_main_py)
-
-sys.path.insert(0, os.path.abspath(absolute_path))
-
-sys.path.append(accert_path)
+sys.path.insert(0, os.path.abspath("../../src"))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
