9e14c0975155bb4ae0ad7f7d90a188a9fd433fc4
jia.zhou <48254033+JiaZhou-PU@users.noreply.github.com>
2026-06-19 10:01:53 -0500

    Fix Windows test import path

--- test/conftest.py
+++ test/conftest.py
@@ -7,6 +7,8 @@ import pandas as pd
 
 PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
 SRC_PATH = os.path.join(PROJECT_ROOT, "src")
+if PROJECT_ROOT not in sys.path:
+    sys.path.insert(0, PROJECT_ROOT)
 if SRC_PATH not in sys.path:
     sys.path.insert(0, SRC_PATH)
 
